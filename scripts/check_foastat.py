e9b6e61e53d5e6c3e763f27cb81e279f1d65038e
uniADunn <ad300@caledonian.ac.uk>
2026-02-26 10:54:23 +0000

    Remove obsolete scripts related to power data processing and spectral analysis
    - Deleted scripts for cleaning, combining, and fetching power data.
    - Removed conversion testing and debugging scripts.
    - Eliminated scripts for extracting and processing FAOSTAT yield data.
    - Cleaned up scripts for generating country centroids and plotting spectral data.
    - Removed scripts for calculating weekly averages and visualizing spectral irradiance for Belgium tomatoes.
    
    MOVED SCRIPTS TO NEW FOLDER AND ADDED IT TO GITIGNORE

--- scripts/check_foastat.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd, zipfile, os
-
-faostat_path = "data/raw/faostat"
-for f in os.listdir(faostat_path):
-    if f.lower().endswith(".zip"):
-        print("Found ZIP:", f)
-
--- scripts/check_power.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-# was removed as this was a test to get the power hourly column names verified.
-df = pd.read_csv("data/raw/power/power_test_valencia_1990-01.csv", skiprows=11)
-print(df.head())
-print(df.columns)
